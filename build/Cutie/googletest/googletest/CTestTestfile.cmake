3b4d59ec6632b8c728a55d7db2f6a5318dc18453
Sunil Bhagwath <sbhagwath@rivian.com>
2020-10-21 22:24:56 -0700

    Initial commit

--- build/Cutie/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sbhagwath/sunilws/gtest-example/Cutie/googletest/googletest
-# Build directory: /home/sbhagwath/sunilws/gtest-example/build/Cutie/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/Cutie/subhook/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sbhagwath/sunilws/gtest-example/Cutie/subhook
-# Build directory: /home/sbhagwath/sunilws/gtest-example/build/Cutie/subhook
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
